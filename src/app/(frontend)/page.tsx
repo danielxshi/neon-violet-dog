dc0451d593640b2c80a9549b7e92a7e2cb943c35
Daniel Shi <danielshi96@gmail.com>
2025-07-29 21:44:44 -0700

    Updating error

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,8 +2,6 @@ import dynamic from 'next/dynamic'
 import PostsSection from './components/post'
 import HomeClient from './components/HomeClient'
 
-// No 'use client' here – this is a server component
-
 export default function HomePage() {
   return (
     <HomeClient>
